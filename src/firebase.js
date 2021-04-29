9b9d6914fb631a79cc7ed627809b945804ddc875
tabanoa <55147915+tabanoa@users.noreply.github.com>
2021-04-29 17:42:55 -0400

    Header set up

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAf88scBnwzq4-eGyEDPgUtt_Po4ps6LZw",
+    authDomain: "clone-fc13a.firebaseapp.com",
+    projectId: "clone-fc13a",
+    storageBucket: "clone-fc13a.appspot.com",
+    messagingSenderId: "498760599875",
+    appId: "1:498760599875:web:d6c0b60ff0f398181d47f6",
+    measurementId: "G-1Y6HTGDV2K"
+  };
