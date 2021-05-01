e58f879d249e5590061c2a4a29ed8a43e08dd019
tabanoa <55147915+tabanoa@users.noreply.github.com>
2021-05-01 12:41:34 -0400

    checkout page created

---
+++ src/Checkout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./Checkout.css";
+import Subtotal from './Subtotal';
+
+
+function Checkout() {
+    return (
+        <div className="checkout">
+          <div className="checkout_left">
+            <img 
+                className="checkout_ad"
+                src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423491668_.jpg"
+                alt=""
+            />
+            <div>
+                <h2 className="checkout_title">Your shopping basket</h2>
+                {/* Basket */}
+                {/* Basket */}
+                {/* Basket */}
+                {/* Basket */}
+                {/* Basket */}
+            </div>
+         </div> 
+         <div className="checkout_right"></div>
+          <Subtotal />
+        </div>
+    );
+}
+
+export default Checkout;
